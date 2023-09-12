672a619976c3245ff6a1179b6564f65b9f5c45ce
mmsc29 <py2425@gmail.com>
2023-09-12 22:43:14 +0800

    電影列表帶入資料&css調整

--- comeseeClient/src/components/home/listPage.jsx
+++ comeseeClient/src/components/home/listPage.jsx
@@ -1,116 +1,159 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 import LS from '../../css/home/listPage.module.css';
 import ListPageCard from './listPageCard';
 
-class ListPage extends Component {
+const ListPage = () => {
 
-    state = {
+    // const [movieData, setMovieData] = useState([]);
+    const [releasedData, setReleasedData] = useState([]);
+    const [comingData, setComingData] = useState([]);
 
-        movie: [
-            {
-                "id": 1, "imageUrl": "https://www.mirrormedia.com.tw/assets/images/20230106145836-8de54a94bc2dc4af5762bc3aef823a1e-tablet.jpg",
-                "movieNameCN": "我的麻吉", "movieNameEN": "Ghost"
-            },
-            {
-                "id": 2, "imageUrl": "https://www.mirrormedia.com.tw/assets/images/20230106145836-8de54a94bc2dc4af5762bc3aef823a1e-tablet.jpg",
-                "movieNameCN": "我的麻吉2", "movieNameEN": "Ghost2"
-            },
-            {
-                "id": 3, "imageUrl": "https://www.mirrormedia.com.tw/assets/images/20230106145836-8de54a94bc2dc4af5762bc3aef823a1e-tablet.jpg",
-                "movieNameCN": "我的麻吉3", "movieNameEN": "Ghost3"
-            },
-            {
-                "id": 4, "imageUrl": "https://www.mirrormedia.com.tw/assets/images/20230106145836-8de54a94bc2dc4af5762bc3aef823a1e-tablet.jpg",
-                "movieNameCN": "134654654", "movieNameEN": "ASDDFADSF"
-            }
-        ]
-
-    }
-
-    render() {
-
-        return (
-            <div className={LS.List} >
-                <div className="container">
+    const [tabReleased, setTabReleased] = useState(true);
+    const [tabComingSoon, setTabComingSoon] = useState(0);
 
 
+    // 後端抓取電影資料
+    // useEffect(() => {
+    //     axios
+    //         .get("http://localhost:2407/filmlist")
+    //         .then((res) => {
+    //             setMovieData(res.data);
+    //         })
+    //         .catch((err) => {
+    //             console.log(err.response);
+    //         });
+    // }, []);
 
-                    <nav className={LS.myNav}>
-                        <div className={LS.myTabs} id="nav-tab" role="tablist">
 
-                            <button
-                                className={`${LS.myLink} listTab active`}
-                                id="nowPlaying-tab"
-                                data-bs-toggle="tab"
-                                data-bs-target="#nowPlaying"
-                                type="button"
-                                role="tab"
-                                aria-controls="nowPlaying"
-                                aria-selected="true"
-                            >
-                                現正熱映
-                            </button>
+    // 選擇上映中標籤
+    function handleReleaseChange(e) {
+        setTabReleased(true);
+        setTabComingSoon(false);
+    };
 
-                            <button
-                                className={`${LS.myLink} listTab`}
-                                id="comingSoon-tab"
-                                data-bs-toggle="tab"
-                                data-bs-target="#comingSoon"
-                                type="button"
-                                role="tab"
-                                aria-controls="comingSoon"
-                                aria-selected="false"
-                            >
-                                即將上映
-                            </button>
-                        </div>
-                    </nav>
+    // 上映中電影
+    useEffect(() => {
+        axios
+            .get("http://localhost:2407/filmlist/released")
+            .then((res) => {
+                setReleasedData(res.data);
+            })
+            .catch((err) => {
+                console.log(err.response);
+            });
+    }, [tabReleased]);
 
-                    <div className={`${LS.myContent} tab-content`} id="nav-tabContent">
+    // 選擇即將上映標籤
+    function handleComingSoonChange(e) {
+        setTabComingSoon(true);
+        setTabReleased(false);
+    };
 
-                        <div
-                            className="tab-pane fade show active"
-                            id="nowPlaying"
-                            role="tabpanel"
-                            aria-labelledby="nowPlaying-tab"
+    // 即將上映電影
+    useEffect(() => {
+        axios
+            .get("http://localhost:2407/filmlist/comingsoon")
+            .then((res) => {
+                setComingData(res.data);
+            })
+            .catch((err) => {
+                console.log(err.response);
+            });
+    }, [tabComingSoon]);
+
+
+
+
+
+
+
+    return (
+        <div className={LS.List} >
+            <div className="container">
+
+
+
+                <nav className={LS.myNav}>
+                    <div className={LS.myTabs} id="nav-tab" role="tablist">
+
+                        <button
+                            className={`${LS.myLink} listTab active`}
+                            id="nowPlaying-tab"
+                            data-bs-toggle="tab"
+                            data-bs-target="#nowPlaying"
+                            type="button"
+                            role="tab"
+                            aria-controls="nowPlaying"
+                            aria-selected="true"
+                            onClick={handleReleaseChange}
                         >
+                            現正熱映
+                        </button>
 
-
-                            {
-                                this.state.movie.map(filmItem =>
-                                    // mapㄉ內容
-                                    <ListPageCard
-                                        key={filmItem.id}
-                                        id={filmItem.id}
-                                        imageUrl={filmItem.imageUrl}
-                                        movieNameCN={filmItem.movieNameCN}
-                                        movieNameEN={filmItem.movieNameEN}
-                                    />
-
-                                )
-                            }
-
-
-                        </div>
-
-                        <div
-                            className="tab-pane fade"
-                            id="comingSoon"
-                            role="tabpanel"
-                            aria-labelledby="comingSoon-tab"
+                        <button
+                            className={`${LS.myLink} listTab`}
+                            id="comingSoon-tab"
+                            data-bs-toggle="tab"
+                            data-bs-target="#comingSoon"
+                            type="button"
+                            role="tab"
+                            aria-controls="comingSoon"
+                            aria-selected="false"
+                            onClick={handleComingSoonChange}
                         >
-                            <ListPageCard />
-                        </div>
+                            即將上映
+                        </button>
+                    </div>
+                </nav>
 
+                <div className={`${LS.myContent} tab-content`} id="nav-tabContent">
+
+                    <div
+                        className="tab-pane fade show active"
+                        id="nowPlaying"
+                        role="tabpanel"
+                        aria-labelledby="nowPlaying-tab"
+                    >
+                        {releasedData.map(filmItem =>
+                            // mapㄉ內容
+                            <ListPageCard
+                                key={filmItem.id}
+                                id={filmItem.id}
+                                imageUrl={filmItem.imageUrl}
+                                movieNameCN={filmItem.movieNameCN}
+                                movieNameEN={filmItem.movieNameEN}
+                            />
+                        )}
                     </div>
 
-
+                    <div
+                        className="tab-pane fade"
+                        id="comingSoon"
+                        role="tabpanel"
+                        aria-labelledby="comingSoon-tab"
+                    >
+                        {comingData.map(filmItem =>
+                            // mapㄉ內容
+                            <ListPageCard
+                                key={filmItem.id}
+                                id={filmItem.id}
+                                imageUrl={filmItem.imageUrl}
+                                movieNameCN={filmItem.movieNameCN}
+                                movieNameEN={filmItem.movieNameEN}
+                            />
+                        )}
+                    </div>
 
                 </div>
+
+
+
             </div>
-        );
-    }
+        </div>
+    );
 }
+
 
 export default ListPage;
