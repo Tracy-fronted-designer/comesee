16804ff9366dbc4ab2da0fb5bd222b61c5ed1ce0
mmsc29 <py2425@gmail.com>
2023-09-13 00:28:36 +0800

    首頁列表帶資料

--- comeseeClient/src/components/home/nowPlaying.js
+++ comeseeClient/src/components/home/nowPlaying.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from "react";
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Link } from 'react-router-dom/cjs/react-router-dom.min';
+import axios from "axios";
 
 import 'swiper/css';
 import 'swiper/css/free-mode';
@@ -8,73 +9,76 @@ import 'swiper/css/navigation';
 import { Navigation, FreeMode } from 'swiper/modules';
 import HS from '../../css/home/homePage.module.css';
 
-class NowPlaying extends Component {
+const NowPlaying = () => {
 
-    state = {
-        movie: [
-            { "imageUrl": "https://s.yimg.com/ny/api/res/1.2/HM0VvYQbSiUijkEIxFNaJQ--/YXBwaWQ9aGlnaGxhbmRlcjt3PTY0MDtoPTk0OA--/https://media.zenfs.com/ko/mirrormedia.mg/153bbf3dd7a3ee6b68f94ca88253faee" },
-            { "imageUrl": "https://s.yimg.com/ny/api/res/1.2/HM0VvYQbSiUijkEIxFNaJQ--/YXBwaWQ9aGlnaGxhbmRlcjt3PTY0MDtoPTk0OA--/https://media.zenfs.com/ko/mirrormedia.mg/153bbf3dd7a3ee6b68f94ca88253faee" },
-            { "imageUrl": "https://s.yimg.com/ny/api/res/1.2/HM0VvYQbSiUijkEIxFNaJQ--/YXBwaWQ9aGlnaGxhbmRlcjt3PTY0MDtoPTk0OA--/https://media.zenfs.com/ko/mirrormedia.mg/153bbf3dd7a3ee6b68f94ca88253faee" },
-            { "imageUrl": "https://s.yimg.com/ny/api/res/1.2/HM0VvYQbSiUijkEIxFNaJQ--/YXBwaWQ9aGlnaGxhbmRlcjt3PTY0MDtoPTk0OA--/https://media.zenfs.com/ko/mirrormedia.mg/153bbf3dd7a3ee6b68f94ca88253faee" },
-            { "imageUrl": "https://s.yimg.com/ny/api/res/1.2/HM0VvYQbSiUijkEIxFNaJQ--/YXBwaWQ9aGlnaGxhbmRlcjt3PTY0MDtoPTk0OA--/https://media.zenfs.com/ko/mirrormedia.mg/153bbf3dd7a3ee6b68f94ca88253faee" },
-            { "imageUrl": "https://s.yimg.com/ny/api/res/1.2/HM0VvYQbSiUijkEIxFNaJQ--/YXBwaWQ9aGlnaGxhbmRlcjt3PTY0MDtoPTk0OA--/https://media.zenfs.com/ko/mirrormedia.mg/153bbf3dd7a3ee6b68f94ca88253faee" },
+    const [slideData, setSlideData] = useState([]);
 
-        ]
-    }
-
-    render() {
-
-        return (
-            <>
-
-                <div className={HS.bar} >
-                    <h1 className={HS.title}>現正熱映 ::</h1>
-                    <Link to="/list"><div className={HS.more}>
-                        看更多
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            width={20}
-                            height={20}
-                            fill="currentColor"
-                            className="bi bi-chevron-right"
-                            viewBox="0 0 16 16"
-                        >
-                            <path
-                                fillRule="evenodd"
-                                d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"
-                            />
-                        </svg>
-                    </div></Link>
-                </div>
+    useEffect(() => {
+        axios
+            .get("http://localhost:2407/filmlist/released")
+            .then((res) => {
+                setSlideData(res.data.slice(0, 10));
+            })
+            .catch((err) => {
+                console.log(err.response);
+            });
+    }, []);
 
 
 
-                <div className={HS.box}>
-                    <Swiper
-                        slidesPerView={5}
-                        spaceBetween={50}
-                        freeMode={true}
-                        navigation={true}
-                        modules={[Navigation, FreeMode]}
-                        className="ListSwiper"
+    return (
+        <>
+
+            <div className={HS.bar} >
+                <h1 className={HS.title}>現正熱映 ::</h1>
+                <Link to="/list"><div className={HS.more}>
+                    看更多
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width={20}
+                        height={20}
+                        fill="currentColor"
+                        className="bi bi-chevron-right"
+                        viewBox="0 0 16 16"
                     >
+                        <path
+                            fillRule="evenodd"
+                            d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"
+                        />
+                    </svg>
+                </div></Link>
+            </div>
 
-                        {this.state.movie.map(filmPoster =>
-                            <SwiperSlide>
-                                <Link to="/info">
-                                    <img className={HS.listSlide}
-                                        src={filmPoster.imageUrl} alt=' ' />
-                                </Link>
-                            </SwiperSlide>
-                        )}
 
-                    </Swiper>
-                    <p className={HS.Btext}>NOW PLAYING</p>
-                </div>
 
-            </>
-        );
-    }
+            <div className={HS.box}>
+                <Swiper
+                    slidesPerView={5}
+                    spaceBetween={50}
+                    freeMode={true}
+                    navigation={true}
+                    modules={[Navigation, FreeMode]}
+                    className="ListSwiper"
+                >
+
+                    {slideData.map(filmPoster =>
+                        <SwiperSlide>
+                            <Link to="/info">
+                                <img
+                                    className={HS.listSlide}
+                                    key={filmPoster.id}
+                                    src={filmPoster.imageUrl} alt=' ' />
+                            </Link>
+                        </SwiperSlide>
+                    )}
+
+                </Swiper>
+                <p className={HS.Btext}>NOW PLAYING</p>
+            </div>
+
+        </>
+    );
 }
+
 
 export default NowPlaying;
