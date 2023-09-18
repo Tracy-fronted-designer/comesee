55c55d78bbec428ed1f33772041f37fd23542213
Lily <t0958654598@gmail.com>
2023-09-18 20:59:10 +0800

    m

--- comeseeClient/src/components/home/recommend.js
+++ comeseeClient/src/components/home/recommend.js
@@ -67,14 +67,14 @@ class Recommend extends Component {
   // 從後端拿到推薦電影
   async componentDidMount() {
     try {
-      if (this.context.state.userID) {
+      if (this.context.state.userID === null) {
         const res = await axios.get(
-          `http://localhost:2407/recommend/home/${this.context.state.userID}`
+          `http://localhost:2407/recommend/home/1`
         );
         console.log(res); //object
         this.setState({ recommendedMovies: res.data }); //data 裡面是 array(電影資料)
       } else {
-        const res = await axios.get(`http://localhost:2407/recommend/home/1`);
+        const res = await axios.get(`http://localhost:2407/recommend/home/${this.context.state.userID}`);
         console.log("userID is not available yet.");
         this.setState({ recommendedMovies: res.data }); //data 裡面是 array(電影資料)
       }
