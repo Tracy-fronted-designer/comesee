6f4f78d2d70f6155fbbccbac0748e8e75a5cd9d8
mmsc29 <py2425@gmail.com>
2023-09-20 15:26:34 +0800

    資訊頁訂票要打包

--- comeseeClient/src/components/home/timeBtn.js
+++ comeseeClient/src/components/home/timeBtn.js
@@ -1,15 +1,21 @@
-import React, { useState, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
+import TicketContext from "../../TicketContext";
 import axios from "axios";
+// import Swal from "sweetalert2";
 
-import TAS from '../../css/home/timetabsComponent.module.css';
-
+import TAS from '../../css/home/timetabsComponent.module.css'
 
 const TimeBtn = (props) => {
 
+    const history = useHistory();
+
+    const { state, setState } = useContext(TicketContext); // 設定使用context
+
     const [showTime, setShowTime] = useState([]);
     const [showTimeID, setShowTimeID] = useState("");
-
     const [checkData, setCheckData] = useState([]);
+    const [selectedValue, setSelectedValue] = useState("");
 
     const movieID = props.movieID;
     const date = props.date;
@@ -55,7 +61,59 @@ const TimeBtn = (props) => {
             });
     }, [showTimeID]);
 
-    // console.log(checkData[0].movieNameCN);
+
+    const handleRadioChange = (event) => {
+        setSelectedValue(event.target.value); // 更新所选的Radio按钮的值
+    };
+
+
+    // 取得showtimeID 跟 使用者選擇數量 更新到context
+    // async function handleShowtimID() {
+
+    //     if (state.userID === null) {
+    //         const result = await Swal.fire({
+    //             title: '請先登入會員',
+    //             icon: 'warning',
+    //             confirmButtonText: "確認",
+    //         });
+
+    //         if (result.isConfirmed) {
+    //             window.location.href = "/login";
+    //         }
+
+    //     } else {
+    //         axios
+    //             .get(`http://localhost:2407/quickorder/emptySeat/${showTimeID}`)
+    //             .then((res) => {
+    //                 const emptySeat = res.data.emptySeat;
+    //                 console.log("空座位數：", emptySeat);
+
+    //                 // 位置數量ok
+    //                 if (parseInt(selectedValue) <= emptySeat) {
+    //                     setState({ showtimeID: showTimeID });
+    //                     setState({ maxSelectedSeats: parseInt(selectedValue) });
+    //                     history.push("/bookingseat");
+    //                     window.scrollTo(0, 0);
+    //                 } else {
+    //                     // 位置數不夠
+    //                     Swal.fire({
+    //                         title: '您目前所選的時段已無空位',
+    //                         text: "請重新選擇",
+    //                         icon: 'warning',
+    //                         confirmButtonText: "確認",
+    //                     })
+    //                     // return;
+    //                 }
+    //             })
+    //             .catch((err) => {
+    //                 console.log(err.response);
+    //             });
+    //     };
+
+    // };
+
+
+
 
 
 
@@ -128,24 +186,30 @@ const TimeBtn = (props) => {
 
                             <h4 className={TAS.numberTitle}>人數</h4>
                             <div className={`${TAS.numberForm} ckmd`}>
-                                <input type="radio" name="number" id="n1" className="btn-check" />
+                                <input type="radio" name="number" id="n1" className="btn-check" value="1" checked={selectedValue === "1"}
+                                    onChange={handleRadioChange} />
                                 <label for="n1" className={`${TAS.numberLabel} btn`}>1</label>
-                                <input type="radio" name="number" id="n2" className="btn-check" />
+                                <input type="radio" name="number" id="n2" className="btn-check" value="2" checked={selectedValue === "2"}
+                                    onChange={handleRadioChange} />
                                 <label for="n2" className={`${TAS.numberLabel} btn`}>2</label>
-                                <input type="radio" name="number" id="n3" className="btn-check" />
+                                <input type="radio" name="number" id="n3" className="btn-check" value="3" checked={selectedValue === "3"}
+                                    onChange={handleRadioChange} />
                                 <label for="n3" className={`${TAS.numberLabel} btn`}>3</label>
-                                <input type="radio" name="number" id="n4" className="btn-check" />
+                                <input type="radio" name="number" id="n4" className="btn-check" value="4" checked={selectedValue === "4"}
+                                    onChange={handleRadioChange} />
                                 <label for="n4" className={`${TAS.numberLabel} btn`}>4</label>
-                                <input type="radio" name="number" id="n5" className="btn-check" />
+                                <input type="radio" name="number" id="n5" className="btn-check" value="5" checked={selectedValue === "5"}
+                                    onChange={handleRadioChange} />
                                 <label for="n5" className={`${TAS.numberLabel} btn`}>5</label>
-                                <input type="radio" name="number" id="n6" className="btn-check" />
+                                <input type="radio" name="number" id="n6" className="btn-check" value="6" checked={selectedValue === "6"}
+                                    onChange={handleRadioChange} />
                                 <label for="n6" className={`${TAS.numberLabel} btn`}>6</label>
                             </div>
 
                         </div>
 
                         <div className={`${TAS.modalfooter} modal-footer`} >
-                            <button className={TAS.modalcheck} type="button">送出</button>
+                            <button className={TAS.modalcheck} type="button" onClick={handleShowtimID} >送出</button>
                         </div>
 
                     </div>
