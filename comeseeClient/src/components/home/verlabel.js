51387e1488c0b5c3cd53e3c701dacb9a1ef6d7a0
mmsc29 <py2425@gmail.com>
2023-09-22 23:32:01 +0800

    標籤顏色更改

--- comeseeClient/src/components/home/verlabel.js
+++ comeseeClient/src/components/home/verlabel.js
@@ -1,29 +1,29 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 
 const verStyle = {
-    width: "110px",
-    height: "40px",
-    borderRadius: "5px",
-    backgroundColor: "#B6B995",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    color: "#F1EFE9",
-    fontFamily: "Noto Sans TC",
-    fontSize: "16px",
-    fontStyle: "normal",
-    fontWeight: "500",
-    lineHeight: "140%"
+  width: "110px",
+  height: "40px",
+  borderRadius: "5px",
+  backgroundColor: "#727366",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  color: "#F1EFE9",
+  fontFamily: "Noto Sans TC",
+  fontSize: "16px",
+  fontStyle: "normal",
+  fontWeight: "500",
+  lineHeight: "140%",
 };
 
 function VerLabel(props) {
-    const { label, id } = props;
+  const { label, id } = props;
 
-    return (
-        <label id={id} style={verStyle}>
-            {label}
-        </label>
-    );
+  return (
+    <label id={id} style={verStyle}>
+      {label}
+    </label>
+  );
 }
 
 export default VerLabel;
