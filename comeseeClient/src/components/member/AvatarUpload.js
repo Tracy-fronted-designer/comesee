228298f36526253ffa89814dbf91b674a45a4709
Tracy <tracy@gmail.com>
2023-09-26 00:06:23 +0800

    m

--- comeseeClient/src/components/member/AvatarUpload.js
+++ comeseeClient/src/components/member/AvatarUpload.js
@@ -1,34 +1,29 @@
 import axios from "axios";
 import React, { useState, useContext, useEffect } from "react";
-//import AvatarEditor from "react-avatar-editor";
 import member from "../../css/member/member.module.css";
 import catchUser from "../../TicketContext";
 
 function AvatarUpload() {
   const [data, setdata] = useState(null);
   const [file, setFile] = useState(null);
-  const [previewImage, setPreviewImage] = useState(null); // 預覽
+  const [previewImage, setPreviewImage] = useState(null);
   const [isFileInputVisible, setIsFileInputVisible] = useState(true);
   const context = useContext(catchUser);
   const user = context.state.userID;
-  //const [scale, setScale] = useState(1);
-  //const fileInputRef = useRef(null);
 
   useEffect(() => {
-    let isMounted = true;
+    getUserImage(); 
+  }, [user]);
+
+  // 抓後端
+  const getUserImage = () => {
     axios
       .get(`http://localhost:2407/user/image/${user}`)
       .then((res) => {
-        if (isMounted) {
-          setdata(res.data[0]);
-          console.log(res.data[0]);
-        }
+        setdata(res.data[0]);
       })
       .catch((err) => console.log(err));
-    return () => {
-      isMounted = false;
-    };
-  }, [user]);
+  };
 
   const handleFile = (e) => {
     const selectedFile = e.target.files[0];
@@ -44,22 +39,27 @@ function AvatarUpload() {
   };
 
   const handleUpload = () => {
-    const formdata = new FormData();
-    formdata.set("image", file);
+    if (!file) {
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append("image", file);
+
     axios
-      .post(`http://localhost:2407/user/uploads/${user}`, formdata)
+      .post(`http://localhost:2407/user/uploads/${user}`, formData)
       .then((res) => {
         console.log(res);
-        // 上傳成功後，更新圖片預覽
-        setPreviewImage(res.data.image); // 假設後端回傳了圖片的 URL
+        // 上傳後，拿到後端Image
+        getUserImage();
+        // 清除後=>再次上傳
+        setFile(null);
+        // 清除預覽
+        setPreviewImage(null);
+        // 顯示文字
+        setIsFileInputVisible(true);
       })
       .catch((err) => console.log(err));
-  };
-
-  const handleImageClick = () => {
-    if (!isFileInputVisible) {
-      document.getElementById("fileInput").click();
-    }
   };
 
   return (
@@ -74,40 +74,31 @@ function AvatarUpload() {
               onChange={handleFile}
               style={{ display: "none" }}
             />
-            <span
-              className={member.AvatarChooseText}
-              onClick={handleImageClick}
-            >
-              選擇照片
-            </span>
+            <span className={member.AvatarChooseText}>選擇照片</span>
           </label>
         </div>
       ) : (
-        <label className={member.Avatarinput}>
+        <div>
+          <label className={member.Avatarinput}>
             <input
-            id="fileInput"
-            className={member.Avatarinput}
-            type="file"
-            onChange={handleFile}
-            style={{ display: "none" }}
-          />
+              id="fileInput"
+              className={member.Avatarinput}
+              type="file"
+              onChange={handleFile}
+              style={{ display: "none" }}
+            />
             <img
-            className={member.Avatarstyle}
-            src={`http://localhost:2407/user/image/${data.image}`}
-            alt="Preview"
-            onClick={handleImageClick}
-          />
-        </label>
+              className={member.Avatarstyle}
+              src={`http://localhost:2407/user/image/${data.image}`}
+              alt="上傳的圖片"
+            />
+          </label>
+        </div>
       )}
 
       {previewImage && (
-        <div className={member.AvatarPreviewContainer}>
-          <img
-            className={member.Avatarstyle}
-            src={`http://localhost:2407/user/image/${data.image}`}
-            alt="
-            "
-          />
+        <div className={member.imageupload}>
+          <img className={member.Avatarstyle} src={previewImage} alt="預覽" />
           <button className={member.customfileinput} onClick={handleUpload}>
             上傳照片
           </button>
